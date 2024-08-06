680837b564c478c0105c9c30c1d00d662e1b9a72
feelamee <fosterlounch@gmail.com>
2024-08-07 00:46:42 +0200

    patch: make SDL2 public dependency of engine for now to handle events

--- orbi/src/main.cpp
+++ orbi/src/main.cpp
@@ -1,6 +1,8 @@
 #include <engine/context.hpp>
 #include <engine/window.hpp>
 
+#include <SDL2/SDL_events.h>
+
 #include <cstdlib>
 
 int
@@ -9,5 +11,19 @@ main()
     using namespace dg;
     context ctx(context::flag::everything);
     window win(ctx, "window", { 480, 295 }, { 960, 590 }, window::flag::shown);
+    while (true)
+    {
+        SDL_Event ev;
+        while (SDL_PollEvent(&ev))
+        {
+            if (ev.type == SDL_QUIT)
+            {
+                return EXIT_SUCCESS;
+            }
+        }
+
+        win.clear_with(0.2, 0.5, 1, 1);
+        win.swap();
+    }
     return EXIT_SUCCESS;
 }
